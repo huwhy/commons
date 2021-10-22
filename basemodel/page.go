87bd6e57806acd87415bb7dcba787d83135db02b
huwhy <huwhy@foxmail.com>
2021-10-22 21:48:20 +0800

    分页支持查询全部

--- basemodel/page.go
+++ basemodel/page.go
@@ -1,10 +1,16 @@
 package basemodel
 
+import "fmt"
+
 type Page struct {
 	*Term
 	Data interface{} `json:"data"`
 }
 
 func NewPage(term *Term, data interface{}) *Page {
+	s := fmt.Sprint(data)
+	if s == "nil" || s == "&[]" || s == "[]" {
+		return &Page{term, []interface{}{}}
+	}
 	return &Page{term, data}
 }
