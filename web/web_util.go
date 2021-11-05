aac9b77365afda9be8391bfae7dd7060c0546650
huwhy <huwhy@foxmail.com>
2021-11-05 11:13:39 +0800

    # 生成代码biz调整 # web包下提交获取请求IP的方法 web.RemoteIp()

---
+++ web/web_util.go
@@ -0,0 +1,23 @@
+package web
+
+import (
+	"net"
+	"net/http"
+)
+
+func RemoteIp(req *http.Request) string {
+	remoteAddr := req.RemoteAddr
+	if ip := req.Header.Get("X-Real-IP"); ip != "" {
+		remoteAddr = ip
+	} else if ip = req.Header.Get("X-Forwarded-For"); ip != "" {
+		remoteAddr = ip
+	} else {
+		remoteAddr, _, _ = net.SplitHostPort(remoteAddr)
+	}
+
+	if remoteAddr == "::1" {
+		remoteAddr = "127.0.0.1"
+	}
+
+	return remoteAddr
+}
