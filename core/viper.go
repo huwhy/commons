00e5b47d4256938eacabdc92385ee9e9e489d241
huwhy <huwhy@foxmail.com>
2022-03-24 10:49:34 +0800

    LoadByConfig

--- core/viper.go
+++ core/viper.go
@@ -80,3 +80,38 @@ func LoadConf(conf config.IConfig) *viper.Viper {
 	}
 	return v
 }
+
+func LoadByConfig(configPath string) (*viper.Viper, *config.Config) {
+	root, _ := filepath.Abs("./")
+	var profile, port string
+	flag.StringVar(&profile, "env", "dev", "启动环境")
+	flag.StringVar(&port, "port", "", "端口")
+	flag.Parse()
+	if configPath == "" {
+		configPath = root + "/config.yml"
+	}
+	var conf = new(config.Config)
+	conf.ROOT = root
+	conf.Profile = profile
+	v := viper.New()
+	v.SetConfigFile(configPath)
+	err := v.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+	v.WatchConfig()
+
+	v.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("config file changed:", e.Name)
+		if err := v.Unmarshal(conf); err != nil {
+			fmt.Println(err)
+		}
+	})
+	if err := v.Unmarshal(conf); err != nil {
+		fmt.Println(err)
+	}
+	if port != "" {
+		conf.Server.Port = port
+	}
+	return v, conf
+}
